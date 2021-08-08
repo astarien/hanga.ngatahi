7adbda5b550b212fbbed7b76d9cf7772b607a904
astarien <37559422+astarien@users.noreply.github.com>
2021-08-08 16:50:13 +1200

    Update PipelineProcessor.py

--- pipeline_classes/processors/PipelineProcessor.py
+++ pipeline_classes/processors/PipelineProcessor.py
@@ -1,1 +1,36 @@
 #
+import implementers, implemented
+
+
+def load.config():
+  
+  config = implemented.ConfigImplemented()
+  
+  return config
+
+def load(config):
+  
+  data = implementers.DataLoader(config)
+  
+  return data
+
+def clean(data):
+  
+  return data
+
+def filter(data):
+  
+  return data
+
+def prepare(data):
+  
+  data = clean(data)
+  data = filter(data)
+  
+  return data
+
+def convert(data):
+  
+  converteddata = implementers.DataConverter(data)
+               
+  
