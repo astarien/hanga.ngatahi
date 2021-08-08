9c7e7d009ab59ce9469df74cd97b93ccb4cead13
astarien <37559422+astarien@users.noreply.github.com>
2021-08-08 17:11:30 +1200

    Update PipelineProcessor.py

--- pipeline_classes/processors/PipelineProcessor.py
+++ pipeline_classes/processors/PipelineProcessor.py
@@ -14,37 +14,40 @@ def load(config):
   
   return data
 
-def clean(data):
+def clean(data, config):
   
   return data
 
-def filter(data):
+def filter(data, config):
   
   return data
 
-def prepare(data):
+def prepare(data, config):
   
-  data = clean(data)
-  data = filter(data)
+  data = clean(data, config)
+  data = filter(data, config)
   
   return data
 
-def convert(data):
+def convert(exported, config):
   
-  converteddata = implementers.DataConverter(data)
-  exported = exportables.Exportable()
+  converteddata = implementers.DataConverter(exported.get(0))
   exported.add(converteddata)
   
   return exported
 
-def report(data, exported):
+def report(exported, config):
   
-  report = implementers.Reporter(data, exported.converted())
+  report = implementers.Reporter(exported.get(0), exported.get(1))
   exported.add(report)
   
   return exported
 
-def export(exported):
+def export(exported, config):
   
+  for i in range(exported.num_items()):
+    exportitem = exported.get(i)
+    # do export
+    
   return
   
